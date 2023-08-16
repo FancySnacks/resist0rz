789fbdb6797dd3dafad6be169a46985ba906c2eb
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-08-16 08:27:26 +0200

    Improved main entry point

--- src/resist0rz/__main__.py
+++ src/resist0rz/__main__.py
@@ -2,39 +2,29 @@
 
 from typing import Optional, Sequence
 
-from resist0rz.Parser import ArgParser
-from resist0rz.ColorCalculator import ColorBandCalculator
+from .App import App
+from .Parser import ArgParser
+from .ColorCalculator import ColorBandCalculator, Calculator
 
 
-def print_resistance(resistor_value, resistance: int, resistance_range: tuple[float, float]):
-    print(f"VALUE: {resistor_value}")
-    print(f"RESISTANCE: {resistance} Ohm")
-    print(f"RANGE: {int(resistance_range[0])} Ohm - {int(resistance_range[1])} Ohm")
+def set_calculator_in_use(calc_type: str) -> Calculator:
+    match calc_type:
+        case 'color': return ColorBandCalculator()
+        case 'smd': return ColorBandCalculator()
+        case default: raise Exception("Invalid resistor type")
 
 
 def main(argv: Optional[Sequence[str]] = None):
-    parser = ArgParser()
-    parsed_args: dict = parser.parse_args(argv)
+    parser = ArgParser(argv)
+    parsed_args = parser.parse_args()
 
-    calculator = None
-    total_resistance = ...
+    calculator = set_calculator_in_use(parsed_args['type'])
+    app = App(calculator)
 
-    if parsed_args['type'] == 'color':
-        calculator = ColorBandCalculator()
-        resistor_value: list[str] = parser.parse_colors(parsed_args['value'])
+    resistor_value: list[str] = parser.parse_colors(parsed_args['value'])
 
-        calculator.multiplier_color = resistor_value.pop(-2)
-        calculator.tolerance_color = resistor_value.pop(-1)
-
-        for color in resistor_value:
-            calculator.add_color(color)
-
-        total_resistance: int = calculator.apply_multiplier(calculator.get_base_resistance_value())
-        resistance_range: tuple[float, float] = calculator.get_tolerance_range(total_resistance)
-    else:
-        pass
-
-    print_resistance(resistor_value, total_resistance, resistance_range)
+    result: dict = app.calculate_resistance(resistor_value)
+    app.print_resistance_value(*result.values())
 
 
 if __name__ == '__main__':
