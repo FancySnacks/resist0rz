57dba41c5df44c1f5431fa08087c432cff586b01
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-08-19 10:14:51 +0200

    Fixed tests to match the refactoring

--- tests/test_ColorCalculator.py
+++ tests/test_ColorCalculator.py
@@ -13,37 +13,25 @@ def test_calculate_base_value(colors, expected_value):
     calculator = ColorBandCalculator()
 
     for color in colors:
-        calculator.add_color(color)
+        calculator.add_value(color)
 
     assert calculator.get_base_resistance_value() == expected_value
 
 
-@pytest.mark.parametrize("colors, multiplier, expected_value", [
-    ([Color.ORANGE, Color.ORANGE, Color.BLACK], Color.BLACK, 330),
-    ([Color.BROWN, Color.BLACK], Color.RED, 1000),
-    ([Color.GREEN, Color.RED, Color.GREEN], Color.BROWN, 5250),
+@pytest.mark.parametrize("colors, expected_value", [
+    ([Color.ORANGE, Color.ORANGE, Color.BLACK, Color.BLACK, Color.GOLD], 330),
+    ([Color.BROWN, Color.BLACK, Color.RED, Color.GOLD], 1000),
+    ([Color.GREEN, Color.RED, Color.GREEN, Color.BROWN, Color.SILVER], 5250),
 ])
-def test_calculate_multiplied_value(colors, multiplier, expected_value):
+def test_calculate_resistance_value(colors, expected_value):
     calculator = ColorBandCalculator()
 
     for color in colors:
-        calculator.add_color(color)
+        calculator.add_value(color)
 
-    calculator.multiplier_color = multiplier
-    base_value = calculator.get_base_resistance_value()
+    result = calculator.get_resistance_values()
 
-    assert calculator.apply_multiplier(base_value) == expected_value
-
-
-def test_multiplier_not_set_returns_base_value():
-    calculator = ColorBandCalculator()
-    calculator.add_color(Color.BROWN)
-    calculator.add_color(Color.BLACK)
-    calculator.add_color(Color.BLACK)
-
-    base_value = calculator.get_base_resistance_value()
-
-    assert calculator.apply_multiplier(base_value) == 100
+    assert result['resistance'][0] == expected_value
 
 
 def test_raises_exception_when_color_has_no_tolerance_value():
@@ -53,20 +41,18 @@ def test_raises_exception_when_color_has_no_tolerance_value():
 
 
 @pytest.mark.parametrize("colors, multiplier, tolerance, expected_value", [
-    ([Color.BROWN, Color.BLACK], Color.BROWN, Color.GOLD, (95.0, 105.0)),
-    ([Color.RED, Color.GREEN], Color.RED, Color.SILVER, (2250.0, 2750.0)),
+    ([Color.BROWN, Color.BLACK], Color.BROWN, Color.GOLD, "95.0 - 105.0"),
+    ([Color.RED, Color.GREEN], Color.RED, Color.SILVER, "2250.0 - 2750.0"),
 ])
 def test_tolerance_is_applied(colors, multiplier, tolerance, expected_value):
     calculator = ColorBandCalculator()
 
     for color in colors:
-        calculator.add_color(color)
+        calculator.add_value(color)
 
     calculator.multiplier_color = multiplier
     calculator.tolerance_color = tolerance
 
-    base_value = calculator.get_base_resistance_value()
-    multiplied_value = calculator.apply_multiplier(base_value)
-    tolerance_range = calculator.get_tolerance_range(multiplied_value)
+    result: dict = calculator.get_resistance_values()
 
-    assert tolerance_range == expected_value
+    assert result['tolerance_range'][0] == expected_value
